8029a7022012f231c6ce07d9ab99260bc74aa47f
Tortique <tortiquedetaharia@gmail.com>
2022-10-19 10:57:22 +0200

    Ajout des tickets et review, modification/suppression

--- authentication/models.py
+++ authentication/models.py
@@ -3,14 +3,5 @@ from django.db import models
 
 
 class User(AbstractUser):
-    CREATOR = 'CREATOR'
-    SUBSCRIBER = 'SUBSCRIBER'
-
-    ROLE_CHOICE = (
-        (CREATOR, 'CREATOR'),
-        (SUBSCRIBER, 'SUBSCRIBER'),
-    )
-
-    profile_photo = models.ImageField()
-    role = models.CharField(max_length=30, choices=ROLE_CHOICE)
+    follows = models.ManyToManyField('self', symmetrical=False)
 
---
+++ litr/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from . import models
+
+User = get_user_model()
+
+
+class TicketForm(forms.ModelForm):
+    edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
+    class Meta:
+        model = models.Ticket
+        fields = ['title', 'description', 'image']
+
+    title = forms.CharField(label="Titre")
+
+
+class DeleteTicketForm(forms.Form):
+    delete_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = ['headline', 'rating', 'body']
+
+
+class DeleteReviewForm(forms.Form):
+    delete_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
